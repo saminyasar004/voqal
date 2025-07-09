030a420404fc530dad50b2bcd7ab9fe234b147c8
saminyasar004 <yasarsamin57@gmail.com>
2025-07-09 15:58:02 +0600

    dev: shadcn component added

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+	({ className, type, ...props }, ref) => {
+		return (
+			<input
+				type={type}
+				className={cn(
+					"flex h-10 w-full rounded-lg bg-white text-primary border border-primary px-3 py-2 text-base file:text-sm file:font-medium file:text-primary placeholder:text-neutral-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm shadow-sm",
+					className
+				)}
+				ref={ref}
+				{...props}
+			/>
+		);
+	}
+);
+Input.displayName = "Input";
+
+export { Input };
