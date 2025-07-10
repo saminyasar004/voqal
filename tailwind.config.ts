6531d982b71cdf9dafe25accdb453775c01d8ebd
saminyasar004 <yasarsamin57@gmail.com>
2025-07-10 17:24:16 +0600

    dev: styling updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,6 @@ export default {
 			fontFamily: {
 				poppins: ["Poppins", "sans-serif"],
 			},
-
 			colors: {
 				primary: "#141414",
 				"primary-foreground": "#fff",
@@ -31,12 +30,22 @@ export default {
 				"golden-olive": "#929208",
 				warning: "#F05F5F",
 				success: "#599862",
+				sidebar: {
+					DEFAULT: "hsl(var(--sidebar-background))",
+					foreground: "hsl(var(--sidebar-foreground))",
+					primary: "hsl(var(--sidebar-primary))",
+					"primary-foreground":
+						"hsl(var(--sidebar-primary-foreground))",
+					accent: "hsl(var(--sidebar-accent))",
+					"accent-foreground":
+						"hsl(var(--sidebar-accent-foreground))",
+					border: "hsl(var(--sidebar-border))",
+					ring: "hsl(var(--sidebar-ring))",
+				},
 			},
-
 			backgroundImage: {
 				hero: "url('./assets/images/hero-bg-pattern.png')",
 			},
-
 			keyframes: {
 				"accordion-down": {
 					from: {
