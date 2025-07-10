52f94f7e7399a5ea3e031bcba9a3f55815081759
saminyasar004 <yasarsamin57@gmail.com>
2025-07-10 20:53:04 +0600

    dev: shadcn component added

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,36 +1,41 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border border-slate-200 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 dark:border-slate-800 dark:focus:ring-slate-300",
-  {
-    variants: {
-      variant: {
-        default:
-          "border-transparent bg-slate-900 text-slate-50 hover:bg-slate-900/80 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/80",
-        secondary:
-          "border-transparent bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
-        destructive:
-          "border-transparent bg-red-500 text-slate-50 hover:bg-red-500/80 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/80",
-        outline: "text-slate-950 dark:text-slate-50",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-)
+	"inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 dark:border-slate-800 dark:focus:ring-slate-300",
+	{
+		variants: {
+			variant: {
+				default:
+					"border-transparent text-primary hover:bg-primary hover:text-white border border-primary",
+				success:
+					"border-transparent text-success hover:bg-success hover:text-white border border-success",
+				warning:
+					"border-transparent text-warning hover:bg-warning hover:text-foreground border border-warning",
+				info: "border-transparent text-blue-500 hover:bg-blue-500 hover:text-white border border-blue-500",
+				secondary:
+					"border-transparent text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+				destructive:
+					"border-transparent text-danger hover:bg-danger hover:text-white border border-danger",
+				outline: "text-slate-950 dark:text-slate-50",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+		},
+	}
+);
 
 export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+	extends React.HTMLAttributes<HTMLDivElement>,
+		VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
-  )
+	return (
+		<div className={cn(badgeVariants({ variant }), className)} {...props} />
+	);
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants };
